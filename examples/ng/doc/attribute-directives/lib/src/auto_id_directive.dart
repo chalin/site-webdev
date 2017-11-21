96649f3bef2e0dea7656d3f9195e668aa590ebd0
Patrice Chalin <pchalin@gmail.com>
2017-11-21 07:29:45 -0500

    Func attr dir UPDATE notes

--- examples/ng/doc/attribute-directives/lib/src/auto_id_directive.dart
+++ examples/ng/doc/attribute-directives/lib/src/auto_id_directive.dart
@@ -2,6 +2,7 @@
 import 'dart:html';
 import 'package:angular/angular.dart';
 
+// FIXME: Use a CounterService
 int _idCounter = 0;
 
 @Directive(selector: '[autoId]')
@@ -19,5 +20,6 @@ as is sometimes done by markdown processors. We can't do that yet because the
 text isn't accessible (no issue was open, it was only discussed via email).
 A fix should be available as of 5.0.0-alpha+1.
 Consider updating this example once this issue is fixed; i.e., if prefix is
-empty then auto-generate the id from the text.
+empty then auto-generate the id from the text:
+  el.text.toLowerCase().replaceAll(r'\A+', '-');
 */
